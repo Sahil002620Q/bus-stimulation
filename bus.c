b3066401bcb82d4114a17d3af2db5e221e18ab73
Sahil002620Q <smax06388@gmail.com>
2026-06-13 22:57:37 +0530

    bus stimulation code first commit --designing stucture and building logic

---
+++ bus.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct bus
+{
+    char passanger[20];
+    int seatnumber[i]; 
+    bool status;
+};
+
+int main() {
+
+
+    
+    int choice;
+    while (true)
+    {
+        //display
+        printf("---Bus reservation stimulator---\n");
+        printf("1. view seats status\n");
+        printf("2. Book seat \n");
+        printf("3. cancel seat\n");
+        printf("4. exit");
+
+        printf("Enter your choice : \n");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 1:
+            printf("available seats ");
+            //1 is free
+            //2 is occupied
+            break;
+
+            case 2:
+            printf("book seat enter name ,seatpreffrence  ");
+            //1 is free
+            //2 is occupied
+            break;
+
+            case 3:
+            printf("seat cancel ,enter name ");
+            //1 is free
+            //2 is occupied
+            break;
+
+            case 4:
+            printf("exit loop ");
+            
+            break;
+        }
+        //end of display
+    }
+    
+
+    struct bus p1;
+    //will not work p1.passanger[20] = "sahil"; 
+    strcpy(p1.passanger,"sahil kumar");
+    printf("passenger name : %s",p1.passanger);
+    printf("booking status : ");
+
+}
