485959b553b506b3e6ea32aebefb0e32065e6924
Sahil002620Q <smax06388@gmail.com>
2026-06-15 01:42:02 +0530

    added cls on multiple option to make cli view more clean , added structure bus seat on top to fix void conflict and replaces all code with void calls

--- bus.c
+++ bus.c
@@ -9,6 +9,8 @@ struct bus
     bool status;
 };
 
+struct bus seat[20]; // max seat
+
 void waitforenter()
 {
     printf("\nEnter to go back to main menu.\n");
@@ -16,9 +18,70 @@ void waitforenter()
     getchar(); //capture enter --non buffered
 }
 
+void viewseats(){
+    system("clear");
+    printf("available seats \n");
+    for (int i = 1; i <= 20; i++)
+    {
+        printf("Seat %d : %s\n", seat[i].seatnumber, seat[i].status ? "booked" : "free");
+    }    
+    waitforenter();
+}
+
+void bookseat(){
+    system("clear");
+    int book;
+    printf("enter seat number you want to book : ", book);
+    scanf("%d", &book);
+    if (book < 1 || book > 20)
+    {
+        printf("Enter a vaild seat number ");
+        waitforenter();
+    }
+    else if (seat[book].status == true)
+    {
+        printf("seat number %d is already booked", book);
+        waitforenter();
+    }
+    else
+    {
+        printf("enter name :");
+        scanf("%s", seat[book].passenger);
+        seat[book].status = true;
+        printf("seat number %d is coniformed by %s\n", seat[book].seatnumber, seat[book].passenger);
+        // logic to book seat
+
+        waitforenter();
+    }
+}
+
+void cancelseat(){
+    system{"clear"};
+    int cancel;
+    printf("Enter seat number, you want to cancel : ");
+    scanf("%d", &cancel);
+
+    if (cancel < 1 || cancel > 20)
+    {
+        printf("The seat doesn't exists\n");
+        waitforenter();
+    }
+    else if (seat[cancel].status == false)
+    {
+        printf("That seat is already vacent\n");
+        waitforenter();
+    }
+    else
+    { 
+        seat[cancel].status = false;
+        printf("cancellation confirmed for seat number %d\n", cancel);
+        waitforenter();
+    }
+}
+
 int main()
 {
-    struct bus seat[20]; // max seat
+    //empty seats
     for (int i = 1; i <= 20; i++)
     {
         seat[i].seatnumber = i; // i+1
@@ -43,61 +106,19 @@ int main()
         switch (choice)
         {
         case 1:
-            printf("available seats \n");
-            for (int i = 1; i <= 20; i++)
-            {
-                printf("Seat %d : %s\n", seat[i].seatnumber, seat[i].status ? "booked" : "free");
-            }    
-            waitforenter();
+            viewseats();
             break;
 
         case 2:
-            int book;
-            printf("enter seat number you want to book :", book);
-            scanf("%d", &book);
-            if (book < 1 || book > 20)
-            {
-                printf("Enter a vaild seat number ");
-            }
-            else if (seat[book].status == true)
-            {
-                printf("seat number %d is already booked", book);
-            }
-            else
-            {
-                printf("enter name :");
-                scanf("%s", seat[book].passenger);
-                seat[book].status = true;
-                printf("seat number %d is coniformed by %s\n", seat[book].seatnumber, seat[book].passenger);
-                // logic to book seat
-
-                waitforenter();
-            }
+            bookseat();
             break;
 
         case 3:
-            int cancel;
-            printf("Enter seat number, you want to cancel : ");
-            scanf("%d", &cancel);
-
-            if (cancel < 1 || cancel > 20)
-            {
-                printf("The seat doesn't exists\n");
-            }
-            else if (seat[cancel].status == false)
-            {
-                printf("That seat is already vacent\n");
-            }
-            else
-            { 
-                seat[cancel].status = false;
-                printf("cancellation confirmed for seat number %d\n", cancel);
-                waitforenter();
-            }
+            cancelseat();
             break;
 
         case 4:
-            printf("exit loop ");
+            printf("exiting loop.... ");
             loop = false;
             break;
         }
